2c7d85c587dc55aa9ac7e651eddd4c2b5809ed8e
Ethan Gardner <edgincvg@gmail.com>
2020-01-21 22:11:31 -0500

    update with touch icons

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const AppManifestWebpackPlugin = require('app-manifest-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
@@ -14,10 +15,25 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new ManifestPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Guitar arpeggios',
+      title: 'Guitar Arpeggio Visualizer',
       template: './src/index.html',
+    }),
+    new ManifestPlugin(),
+    new AppManifestWebpackPlugin({
+      logo: './src/touch-icon.png', // svg works too!
+      prefix: '/demo/guitar-arpeggios',
+      inject: true,
+      config: {
+        appName: 'Guitar Arpeggio Visualizer',
+        background: '#ecf4fb',
+        theme_color: '#068a9c',
+        icons: {
+          firefox: false,
+          coast: false,
+          yandex: false
+        }
+      }
     }),
     new WorkboxPlugin.GenerateSW({
       swDest: 'sw.js',
